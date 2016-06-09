0c37602a80858f9a5f5b156653cdb26183f956c3
Alexandre Poirot <poirot.alex@gmail.com>
2016-06-09 05:16:17 -0700

    Set default url to the landing-page.

--- browsers/advanced/js/tabiframedeck.js
+++ browsers/advanced/js/tabiframedeck.js
@@ -19,7 +19,7 @@ define(['js/tabiframe', 'js/eventemitter', 'js/keybindings'],
 
   'use strict';
 
-  const HOMEPAGE = 'https://github.com/paulrouget/firefox.html/commits/master';
+  const HOMEPAGE = '/landing-page/';
 
   let _tabIframeArray = [];
   let _selectIndex = -1;
