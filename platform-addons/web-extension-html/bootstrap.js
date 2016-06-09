7a399f8810b5143734aa733f52c86bb87ea2da89
Alexandre Poirot <poirot.alex@gmail.com>
2016-06-09 14:12:59 -0700

    Fix early startup of devtools

--- platform-addons/web-extension-html/bootstrap.js
+++ platform-addons/web-extension-html/bootstrap.js
@@ -3,35 +3,7 @@ dump("bootstrap\n");
 const Cu = Components.utils;
 const Ci = Components.interfaces;
 
-// Register the manifest early, so the override in chrome.manifest
-// takes effect before web extensions scripts are inserted into the
-// addon scope.
-(function loadManifest() {
-  Cu.import("resource://gre/modules/Services.jsm");
-  let manifest = Services.dirsvc.get('ProfD', Ci.nsIFile);
-  manifest.append('extensions');
-  manifest.append('zyzzyva');
-  Components.manager.addBootstrappedManifestLocation(manifest);
-})();
-
-function waitForWindow() {
-  let window = Services.wm
-                       .getMostRecentWindow(null);
-  if (window) {
-    return Promise.resolve(window);
-  }
-  return new Promise(done => {
-    Services.wm.addListener({
-      onOpenWindow: function(aXULWindow) {
-        Services.wm.removeListener(this);
-        // We have to wait for a tick for getMostRecentWindow to return the window
-        Services.tm.mainThread.dispatch(function() {
-          done(waitForWindow());
-        }, Components.interfaces.nsIThread.DISPATCH_NORMAL);
-      }
-    });
-  });
-}
+Cu.import("resource://gre/modules/Services.jsm");
 
 function startup() {
   Cu.import("resource://gre/modules/ExtensionManagement.jsm");
@@ -42,22 +14,19 @@ function startup() {
   // in testing/marionette/driver.js
   // Note that it also starts devtools.
   Cu.import("resource://webextensions/glue.jsm");
-  // We wait for the window to be opened to register the devtools and especially
-  // its about:devtools-toolbox URI.
-  waitForWindow().then(window => {
+  WindowUtils.getWindow().then(window => {
     Services.obs.notifyObservers(window, "browser-delayed-startup-finished", null);
   });
+
+  // Init devtools so that about:devtools-toolbox works asap
+  let { loader } = Cu.import("resource://devtools/shared/Loader.jsm", {});
+  // Ensure loading main devtools module that hooks up into browser UI
+  // and initialize all devtools machinery.
+  loader.require("devtools/client/framework/devtools-browser");
 }
 
 function install() {
 }
 
 function shutdown() {
-  (function unloadManifest() {
-    Cu.import("resource://gre/modules/Services.jsm");
-    let manifest = Services.dirsvc.get('ProfD', Ci.nsIFile);
-    manifest.append('extensions');
-    manifest.append('zyzzyva');
-    Components.manager.removeBootstrappedManifestLocation(manifest);
-  })();
 }
