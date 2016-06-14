a9789dc8e06e9b2354aefa02067264477cfd3429
Alexandre Poirot <poirot.alex@gmail.com>
2016-06-14 06:07:52 -0700

    Install addons from path instead of xpi to allow reloading from sources easily.

--- landing-page/js/addons.js
+++ landing-page/js/addons.js
@@ -1,13 +1,16 @@
 addEventListener("load", function () {
-  preloadAddons();
+  updateAddonState();
 
   let group = document.querySelector(".group.addons");
   group.addEventListener("click", function(event) {
     let { id, path } = event.target.dataset;
     if (!id) return;
     if (event.target.classList.contains("keep-installed")) {
-      disable(id).then(() => {
+      if (event.target.processing) return;
+      event.target.processing = true;
+      disable(path, id).then(() => {
         event.target.classList.remove("enabled");
+        event.target.processing = false;
       });
     }
     event.target.classList.toggle("keep-installed");
@@ -16,9 +19,14 @@ addEventListener("load", function () {
   group.addEventListener("mouseover", function(event) {
     let { id, path } = event.target.dataset;
     if (!id) return;
-    enable(id).then(() => {
-      event.target.classList.add("enabled");
-    });
+    if (!event.target.classList.contains("enabled")) {
+      if (event.target.processing) return;
+      event.target.processing = true;
+      enable(path, id).then(() => {
+        event.target.classList.add("enabled");
+        event.target.processing = false;
+      });
+    }
   });
 
   group.addEventListener("mouseout", function(event) {
@@ -27,43 +35,36 @@ addEventListener("load", function () {
     if (event.target.classList.contains("keep-installed")) {
       return;
     }
-    disable(id).then(() => {
-      event.target.classList.remove("enabled");
-    });
+    if (event.target.classList.contains("enabled")) {
+      if (event.target.processing) return;
+      event.target.processing = true;
+      disable(path, id).then(() => {
+        event.target.classList.remove("enabled");
+        event.target.processing = false;
+      });
+    }
   });
 });
 
-function preloadAddons() {
+function updateAddonState() {
   for(let addon of document.querySelectorAll(".group.addons li")) {
     let extension = addon;
     let { id, path } = addon.dataset;
-    maybeInstall(path, id).then((enabled) => {
-      extension.classList.add("installed");
-      extension.classList.toggle("enabled", enabled);
-    });
+    isInstalled(id).then(function (addon, installed) {
+      if (installed) {
+        addon.classList.add("enabled");
+        addon.classList.add("keep-installed");
+      } else {
+        addon.classList.remove("enabled");
+      }
+      addon.classList.add("loaded");
+    }.bind(null, addon));
   }
 }
 
 let am = navigator.mozAddonManager;
 
 let cache = {};
-
-function maybeInstall(path, id) {
-  return new Promise(function(resolve, reject) {
-    am.getAddonByID(id).then(
-     function(addon) {
-        if (addon) {
-          resolve(addon.isEnabled);
-        } else {
-          install(path).then(() => {
-            disable(id);
-            resolve(false);
-          });
-        }
-      }
-    );
-  });
-}
 
 function getAddonByID(id) {
   return new Promise(function(resolve, reject) {
@@ -88,98 +89,41 @@ function getAddonByID(id) {
   });
 }
 
-function install(path) {
+let channel = new BroadcastChannel("addons");
+
+function isInstalled(id) {
   return new Promise(done => {
-    let url = new URL(path, location);
-    am.createInstall({url}).then(
-      function success(installer) {
-
-        let events = [
-          "onDownloadStarted",
-          "onDownloadProgress",
-          "onDownloadEnded",
-          "onDownloadCancelled",
-          "onDownloadFailed",
-          "onInstallStarted",
-          "onInstallEnded",
-          "onInstallCancelled",
-          "onInstallFailed",
-        ];
-
-        events.forEach(function(name) {
-          installer.addEventListener(name, function() {
-            if (name == "onInstallEnded") done();
-          });
-        });
-
-        installer.install();
-      },
-      function failure() {
-        console.log('Failed to createInstall for: ', url);
+    channel.addEventListener("message", function onMessage({ data }) {
+      if (data.event == "isInstalledResponse" && data.id == id) {
+        channel.removeEventListener("message", onMessage);
+        done(data.isInstalled);
       }
-    );
+    });
+    channel.postMessage({ event: "isInstalled", id });
   });
 }
 
-function uninstall(id) {
-  getAddonByID(id).then(
-    function succes(addon) {
-      addon.uninstall().then(
-        function success() {
-          console.log('success to call uninstall for :', id);
-        },
-        function failure() {
-          console.log('Fail to call uninstall for :', id);
-        }
-      );
-    },
-
-    function failure() {
-      console.log('Fail to call getAddonById for :', id);
-    }
-  );
-}
-
-function disable(id) {
+function enable(path, id) {
   return new Promise(done => {
-    getAddonByID(id).then(
-      function succes(addon) {
-        addon.disable().then(
-          function success() {
-            done();
-            console.log('success to call disable for :', id);
-          },
-          function failure() {
-            console.log('Fail to call disable for :', id);
-          }
-        );
-      },
-
-      function failure() {
-        console.log('Fail to call getAddonById for :', id);
+    let url = new URL(path, location).href;
+    channel.addEventListener("message", function onMessage({ data }) {
+      if (data.event == "installed" && data.id == id) {
+        channel.removeEventListener("message", onMessage);
+        done();
       }
-    );
+    });
+    channel.postMessage({ event: "install", url, id });
   });
 }
 
-function enable(id) {
+function disable(path, id) {
   return new Promise(done => {
-    getAddonByID(id).then(
-      function succes(addon) {
-        addon.enable().then(
-          function success() {
-            done();
-            console.log('success to call enable for :', id);
-          },
-          function failure() {
-            console.log('Fail to call enable for :', id);
-          }
-        );
-      },
-
-      function failure() {
-        console.log('Fail to call getAddonById for :', id);
+    channel.addEventListener("message", function onMessage({ data }) {
+      if (data.event == "uninstalled" && data.id == id) {
+        channel.removeEventListener("message", onMessage);
+        done();
       }
-    );
+    });
+    channel.postMessage({ event: "uninstall", id });
   });
 }
--- platform-addons/web-extension-html/bootstrap.js
+++ platform-addons/web-extension-html/bootstrap.js
@@ -23,10 +23,77 @@ function startup() {
   // Ensure loading main devtools module that hooks up into browser UI
   // and initialize all devtools machinery.
   loader.require("devtools/client/framework/devtools-browser");
+
+  setupAddons();
 }
 
 function install() {
 }
 
 function shutdown() {
+}
+
+let Addons = [];
+let AddonInstances = new Map();
+try {
+  Addons = Services.prefs.getCharPref("webextensions.list");
+  if (!Addons || !Array.isArray(Addons)) {
+    Addons = [];
+  }
+} catch(e) {}
+
+
+let windows = [];
+function BroadcastChannelFor(uri, name) {
+  let baseURI = Services.io.newURI(uri, null, null);
+  let principal = Services.scriptSecurityManager.createCodebasePrincipal(baseURI, {inIsolatedMozBrowser:true});
+
+  let chromeWebNav = Services.appShell.createWindowlessBrowser(true);
+  // XXX: Keep a ref to the window otherwise it is garbaged and BroadcastChannel stops working.
+  windows.push(chromeWebNav);
+  let interfaceRequestor = chromeWebNav.QueryInterface(Ci.nsIInterfaceRequestor);
+  let docShell = interfaceRequestor.getInterface(Ci.nsIDocShell);
+  docShell.createAboutBlankContentViewer(principal);
+  let window = docShell.contentViewer.DOMDocument.defaultView;
+  return new window.BroadcastChannel(name);
+}
+function setupAddons() {
+  let chromeURL = Services.prefs.getCharPref("browser.chromeURL");
+  let channel = BroadcastChannelFor(chromeURL, "addons");
+  channel.addEventListener("message", ({ data }) => {
+    if (data.event == "install") {
+      installAddon(data);
+      Addons.push(data);
+      channel.postMessage({ event: "installed", id: data.id });
+    }
+    else if (data.event == "uninstall") {
+      let idx = Addons.indexOf(data);
+      if (idx != -1) {
+        Addons.splice(idx, 1);
+      }
+
+      let addon = AddonInstances.get(data.id);
+      addon.shutdown();
+      AddonInstances.delete(data.id);
+      channel.postMessage({ event: "uninstalled", id: data.id });
+    }
+    else if (data.event == "isInstalled") {
+      let isInstalled = AddonInstances.has(data.id);
+      channel.postMessage({ event: "isInstalledResponse", isInstalled, id: data.id });
+    }
+    Services.prefs.setCharPref("webextensions.list", JSON.stringify(Addons));
+  });
+
+  Addons.forEach(installAddon);
+}
+
+let {Extension} = Components.utils.import("resource://gre/modules/Extension.jsm", {});
+function installAddon(addon) {
+  let data = {
+    id: addon.id,
+    resourceURI: Services.io.newURI(addon.url, null, null)
+  };
+  let extension = new Extension(data);
+  extension.startup();
+  AddonInstances.set(addon.id, extension);
 }
