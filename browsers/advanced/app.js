6b8d1048648549209f8e8895131c488b20df38cc
Alexandre Poirot <poirot.alex@gmail.com>
2016-06-18 04:31:41 -0700

    Make aeroglass to work on windows.

--- browsers/advanced/app.js
+++ browsers/advanced/app.js
@@ -13,15 +13,15 @@
 
 if (navigator.appVersion.indexOf('Win') >= 0) {
   window.OS = 'windows';
-  document.body.setAttribute('os', 'windows');
+  document.documentElement.setAttribute('os', 'windows');
 }
 if (navigator.appVersion.indexOf('Mac') >= 0) {
   window.OS = 'osx';
-  document.body.setAttribute('os', 'osx');
+  document.documentElement.setAttribute('os', 'osx');
 }
 if (navigator.appVersion.indexOf('X11') >= 0) {
   window.OS = 'linux';
-  document.body.setAttribute('os', 'linux');
+  document.documentElement.setAttribute('os', 'linux');
 }
 
 // IS_PRIVILEGED is false if Firefox.html runs in a regular browser,
